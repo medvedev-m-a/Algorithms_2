a6ddcd980d3a592d48455f62313c2b5defad574b
Maxim Medvedev <m.a.medvedev@outlook.com>
2022-10-17 00:32:29 +0500

    add algorythms_2_1_trees.py.py

---
+++ algorythms_2_1_trees.py
@@ -0,0 +1,79 @@
+class SimpleTreeNode:
+
+    def __init__(self, val, parent):
+        self.NodeValue = val  # значение в узле
+        self.Parent = parent  # родитель или None для корня
+        self.Children = []  # список дочерних узлов
+
+    def __str__(self):
+        return f"TreeNode({self.NodeValue})"
+
+
+class SimpleTree:
+
+    def __init__(self, root):
+        self.Root = root  # корень, может быть None
+
+    def AddChild(self, ParentNode, NewChild):
+        if self.Root is None:
+            self.Root = NewChild
+        else:
+            NewChild.Parent = ParentNode
+            ParentNode.Children.append(NewChild)
+
+    def DeleteNode(self, NodeToDelete):
+        NodeToDelete.Parent = None
+        if NodeToDelete.Children:
+            for Child in NodeToDelete.Children:
+                self.DeleteNode(Child)
+
+    def GetAllNodes(self):
+        all_nodes = []
+        if self.Root.Children:
+            self.GetAllChild(all_nodes, self.Root)
+        return all_nodes
+
+    def GetAllChild(self, all_nodes, node):
+        all_nodes.append(node)
+        if node.Children:
+            for child in node.Children:
+                self.GetAllChild(all_nodes, child)
+        return all_nodes
+
+    def FindNodesByValue(self, val):
+        found_nodes = []
+        if self.Root.Children:
+            self.FindValue(found_nodes, val, self.Root)
+        return found_nodes
+
+    def FindValue(self, found_nodes, val, node):
+        if node.NodeValue == val:
+            found_nodes.append(node)
+        if node.Children:
+            for child in node.Children:
+                self.FindValue(found_nodes, val, child)
+        return found_nodes
+
+    def MoveNode(self, OriginalNode, NewParent):
+        if OriginalNode == self.Root:
+            return
+        OriginalNode.Parent.Children.remove(OriginalNode)
+        OriginalNode.Parent = NewParent
+        NewParent.Children.append(OriginalNode)
+
+    def Count(self):
+        return len(self.GetAllNodes())
+
+    def LeafCount(self):
+        all_leaf = []
+        if self.Root:
+            self.FindLeafs(all_leaf, self.Root)
+        return len(all_leaf)
+
+    def FindLeafs(self, all_leaf, node):
+        if node.Children:
+            for child in node.Children:
+                self.FindLeafs(all_leaf, child)
+        else:
+            all_leaf.append(node)
+        return all_leaf
---
+++ algorythms_2_1_trees_tests.py
@@ -0,0 +1,41 @@
+from algorythms_2_1_trees import SimpleTree, SimpleTreeNode
+
+a1 = SimpleTreeNode(9, None)
+a2 = SimpleTreeNode(4, None)
+a3 = SimpleTreeNode(17, None)
+a4 = SimpleTreeNode(3, None)
+a5 = SimpleTreeNode(6, None)
+a6 = SimpleTreeNode(22, None)
+a7 = SimpleTreeNode(5, None)
+a8 = SimpleTreeNode(3, None)
+
+tree = SimpleTree(a1)
+tree.AddChild(a1, a2)
+print(tree.LeafCount())
+
+tree.AddChild(a1, a3)
+print(tree.LeafCount())
+
+tree.AddChild(a2, a4)
+tree.AddChild(a2, a5)
+tree.AddChild(a3, a6)
+print(tree.LeafCount())
+
+
+tree.AddChild(a5, a7)
+
+all_nodes = tree.GetAllNodes()
+print(all_nodes)
+print(len(all_nodes))
+print(tree.Count())
+
+print(tree.FindNodesByValue(3))
+tree.AddChild(a7, a8)
+print(tree.FindNodesByValue(3))
+tree.MoveNode(a7, a6)
+print(tree.LeafCount())
+
+print(a1)
+print(a2)
+
+tree.DeleteNode(a4)
